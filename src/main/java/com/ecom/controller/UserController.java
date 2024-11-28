2a59c22d80bbdcb7a3ec0f17ca3b6f5c7bc27693
nghia <nguyentrongnghia.kt82@gmail.com>
2024-11-28 14:02:23 +0700

    Add&View-Cart

--- src/main/java/com/ecom/controller/UserController.java
+++ src/main/java/com/ecom/controller/UserController.java
@@ -6,15 +6,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ecom.model.Cart;
 import com.ecom.model.Category;
+import com.ecom.model.OrderRequest;
 import com.ecom.model.UserDtls;
+import com.ecom.service.CartService;
 import com.ecom.service.CategoryService;
-import com.ecom.service.ProductService;
+import com.ecom.service.OrderService;
 import com.ecom.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -29,10 +37,13 @@ public class UserController {
 	private CategoryService categoryService;
 
 	@Autowired
-	private ProductService productService;
+	private UserService userService;
 
 	@Autowired
-	private UserService userService;
+	private CartService cartService;
+
+	@Autowired
+	private OrderService orderService;
 
 	@ModelAttribute
 	public void getUserDetails(Principal p, Model m) {
@@ -40,9 +51,81 @@ public class UserController {
 			String email = p.getName();
 			UserDtls userDtls = userService.getUserByEmail(email);
 			m.addAttribute("user", userDtls);
+			Integer countCart = cartService.getCountCart(userDtls.getId());
+			m.addAttribute("countCart", countCart);
 		}
 		List<Category> allActiveCategory = categoryService.getAllActiveCategory();
 		m.addAttribute("categorys", allActiveCategory);
 
+	}
+
+	@GetMapping("/addCart")
+	public String addToCart(@RequestParam Integer pid, @RequestParam Integer uid, HttpSession session) {
+
+		Cart saveCart = cartService.saveCart(pid, uid);
+
+		if (ObjectUtils.isEmpty(saveCart)) {
+			session.setAttribute("errorMsg", "Product add to cart failed!");
+		} else {
+			session.setAttribute("errorMsg", "Product added to cart!");
+		}
+
+		return "redirect:/product/" + pid;
+	}
+
+	@GetMapping("/cart")
+	public String loadCartPage(Principal p, Model m) {
+		UserDtls user = getLoggedInUserDetails(p);
+		List<Cart> carts = cartService.getCartsByUser(user.getId());
+		m.addAttribute("carts", carts);
+		if (carts.size() > 0) {
+			Double totalOrderPrice = carts.get(carts.size() - 1).getTotalOrderPrice();
+			m.addAttribute("totalOrderPrice", totalOrderPrice);
+		}
+		return "/user/cart";
+	}
+
+	@GetMapping("/cartQuantityUpdate")
+	public String updateCartQuantity(@RequestParam String sy, @RequestParam Integer cid) {
+		cartService.updateQuantity(sy, cid);
+
+		return "redirect:/user/cart";
+	}
+
+	public UserDtls getLoggedInUserDetails(Principal p) {
+		String email = p.getName();
+		UserDtls userDtls = userService.getUserByEmail(email);
+
+		return userDtls;
+	}
+
+	// Code Order
+
+	@GetMapping("/orders")
+	public String orderPage(Principal p, Model m) {
+		UserDtls user = getLoggedInUserDetails(p);
+		List<Cart> carts = cartService.getCartsByUser(user.getId());
+		m.addAttribute("carts", carts);
+		if (carts.size() > 0) {
+			Double orderPrice = carts.get(carts.size() - 1).getTotalOrderPrice();
+			Double totalOrderPrice = carts.get(carts.size() - 1).getTotalOrderPrice() + 250 + 100;
+			m.addAttribute("orderPrice", orderPrice);
+			m.addAttribute("totalOrderPrice", totalOrderPrice);
+		}
+		return "/user/order";
+	}
+
+	@PostMapping("/save-order")
+	public String savePage(@ModelAttribute OrderRequest request, Principal p) {
+		// System.out.println(request);
+
+		UserDtls user = getLoggedInUserDetails(p);
+		orderService.saveOrder(user.getId(), request);
+		return "redirect:/user/success";
+	}
+
+	@GetMapping("/success")
+	public String loadSuccess() {
+		return "/user/success";
 	}
 }
