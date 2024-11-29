a3faf3f6da9fc426e8ea27a329e615816f782799
nghia <nguyentrongnghia.kt82@gmail.com>
2024-11-30 00:48:30 +0700

    view-product-admin

--- src/main/java/com/ecom/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ecom/service/impl/ProductServiceImpl.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -101,6 +103,24 @@ public class ProductServiceImpl implements ProductService {
 		}
 
 		return products;
+	}
+
+	@Override
+	public List<Product> searchProduct(String ch) {
+		return productRepository.findByTitleContainingIgnoreCaseOrCategoryContainingIgnoreCase(ch, ch);
+	}
+
+	@Override
+	public Page<Product> getAllActiveProductPagination(Integer pageNo, Integer pageSize, String category) {
+		PageRequest pageable = PageRequest.of(pageNo, pageSize);
+		Page<Product> pageProduct = null;
+		if (ObjectUtils.isEmpty(category)) {
+			pageProduct = productRepository.findByIsActiveTrue(pageable);
+		} else {
+			pageProduct = productRepository.findByCategory(pageable, category);
+		}
+
+		return pageProduct;
 	}
 
 }
