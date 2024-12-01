acd5ea8526377423939c5052823239628a24058e
nghia <nguyentrongnghia.kt82@gmail.com>
2024-12-01 11:53:49 +0700

    add-admin

--- src/main/java/com/ecom/service/ProductService.java
+++ src/main/java/com/ecom/service/ProductService.java
@@ -25,4 +25,10 @@ public interface ProductService {
 	public List<Product> searchProduct(String ch);
 
 	public Page<Product> getAllActiveProductPagination(Integer pageNo, Integer pageSize, String category);
+
+	public Page<Product> searchProductPagination(Integer pageNo, Integer pageSize, String ch);
+
+	public Page<Product> getAllProductsPagination(Integer pageNo, Integer pageSize);
+
+	public Page<Product> searchActiveProductPagination(Integer pageNo, Integer pageSize, String category, String ch);
 }
